6d98e4885f8830fde9812f96e94eb97404b758ed
kaznad <kazak21@gmail.com>
2019-02-01 17:09:07 +0800

    Update prometheus.sh

--- scripts/prometheus.sh
+++ scripts/prometheus.sh
@@ -24,3 +24,13 @@ aws s3 cp s3://md-sample/prometheus.service /etc/systemd/system/prometheus.servi
 systemctl daemon-reload
 systemctl start prometheus
 systemctl enable prometheus
+cd ~
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.15.1/node_exporter-0.15.1.linux-amd64.tar.gz
+tar xvf node_exporter-0.15.1.linux-amd64.tar.gz
+cp node_exporter-0.15.1.linux-amd64/node_exporter /usr/local/bin
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-0.15.1.linux-amd64.tar.gz node_exporter-0.15.1.linux-amd64
+aws s3 cp s3://md-sample/node_exporter.service /etc/systemd/system/node_exporter.service
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter
