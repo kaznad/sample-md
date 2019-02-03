c8b87637039b527c88a8a56734134c66a833df69
kaznad <kazak21@gmail.com>
2019-02-03 13:47:38 +0800

    Create build_grafana.sh

---
+++ scripts/build_grafana.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+aws s3 cp s3://md-sample/grafana.repo /etc/yum.repos.d/grafana.repo
+yum install grafana -y
+systemctl daemon-reload
+systemctl start grafana-server
+systemctl status grafana-server
