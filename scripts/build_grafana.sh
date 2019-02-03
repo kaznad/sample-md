201332463fe90830c618fd0605ebae18859f5569
kaznad <kazak21@gmail.com>
2019-02-03 16:31:20 +0800

    Update build_grafana.sh

--- scripts/build_grafana.sh
+++ scripts/build_grafana.sh
@@ -3,4 +3,4 @@ aws s3 cp s3://md-sample/grafana.repo /etc/yum.repos.d/grafana.repo
 yum install grafana -y
 systemctl daemon-reload
 systemctl start grafana-server
-systemctl status grafana-server
+systemctl enable grafana-server
